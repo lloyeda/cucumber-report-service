a4c3cf675779bcce5dd98520af6300f662623ee5
lloyeda <alloyed@hotmail.com>
2020-05-16 17:30:41 +0100

    Initial commit

---
+++ src/main/java/com/lloyeda/cucumberreportservice/CucumberReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lloyeda.cucumberreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lloyeda/cucumberreportservice/CucumberReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloyeda.cucumberreportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
